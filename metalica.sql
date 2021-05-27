3eb6333b9ceb5b5e9ffc4fe3ca98d06ad7139676
pratiksha <rupa25shinde@gmail.com>
2021-05-27 18:47:35 +0530

    sql added

--- metalica.sql
+++ metalica.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 27, 2021 at 03:03 PM
+-- Generation Time: May 27, 2021 at 03:09 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.2.34
 
